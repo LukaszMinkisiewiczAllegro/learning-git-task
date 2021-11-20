d9b21bf9d2dd546f1d4ef8028a6f44d0d60f2be0
Lukasz Minkisiewicz <lukasz.minkisiewicz@allegro.pl>
2021-11-20 02:11:08 +0100

    End program

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,2 +1,8 @@
 zakupy = {'Piekarnia': ['chleb', 'bułki', 'pączke'], 'Warzywniak': ['marchew','seler','rukola']}
 
+x = 0
+for sklep, lista in zakupy.items():
+    print(f'Idę do {sklep.upper()}, aby kupić: {lista}')
+    x += len(lista)
+
+print(f'W sumię kupię {x} przedmiotów')
